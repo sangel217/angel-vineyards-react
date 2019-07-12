61e021fffae7c35f400ab5ce4df402bba8553699
Sarah Angel <sangel217@hotmail.com>
2019-07-12 12:02:39 -0700

    add admin to handle creating new wines and implemented state in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,37 @@ import NavBar from './NavBar';
 import Menu from './Menu';
 import WineForm from './WineForm';
 import { Switch, Route } from 'react-router-dom';
+import { v4 } from "uuid";
 
-function App(){
-  return (
-    <div>
-      <NavBar/>
-      <Switch>
-        <Route exact path='/' component={Menu} />
-        <Route path='/newwine' component={WineForm} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      masterWineList: {}
+    };
+    this.handleAddingNewWineToList = this.handleAddingNewWineToList.bind(this);
+  }
+
+  handleAddingNewWineToList(newWine){
+    newWineId = v4();
+    var newWineList = Object.assign({}, this.state.masterWineList, {
+      [newWineId]: newWine 
+    });
+    this.setState({masterWineList: newWineList});
+  }
+
+  render(){
+    return (
+      <div>
+        <NavBar/>
+        <Switch>
+          <Route exact path='/' render={()=><WineList wineList={this.state.masterWineList} />} />
+          <Route path='/newwine' component={WineForm} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
