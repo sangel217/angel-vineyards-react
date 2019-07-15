26ad88d8d13f8d1bb01f6078ec9586e9b9021244
Student <example@example.com>
2019-07-15 13:29:20 -0700

    add admin link to nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar(){
   return(
     <div>
       <h1>Welcome to Angel Vineyards Estate</h1>
-      <Link to="/">Menu</Link> | <Link to="/newwine">Add New Wine</Link>
+      <Link to="/">Menu</Link> | <Link to="/newwine">Add New Wine</Link> | <Link to="/admin">Admin</Link>
     </div>
   );
 }
